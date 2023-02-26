c826f45a63af3cc5afe89193721ba5f2a6011319
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2023-02-26 21:43:11 +0530

    Add main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+class Student:
+    def __init__(self, name, roll_no, major, year):
+        self.name = name
+        self.roll_no = roll_no
+        self.major = major
+        self.year = year
+
+    def __repr__(self):
+        return f"Student: {self.name} {self.roll_no} {self.major} {self.year}"
+
+    def __eq__(self, another):
+        if self.__class__ == another.__class__:
+            return (self.name, self.roll_no, self.major, self.year) == (
+                another.name,
+                another.roll_no,
+                another.major,
+                another.year,
+            )
+        else:
+            return "InvalidComparison"
