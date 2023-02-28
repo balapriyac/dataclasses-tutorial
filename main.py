e0f04e8dae0360593542d3efc57557a5d5d60fe0
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2023-02-28 09:57:18 +0530

    Add data class

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+# class Student:
+#     def __init__(self, name, roll_no, major, year):
+#         self.name = name
+#         self.roll_no = roll_no
+#         self.major = major
+#         self.year = year
+
+#     def __repr__(self):
+#         return f"Student: {self.name} {self.roll_no} {self.major} {self.year}"
+
+#     def __eq__(self, another):
+#         if self.__class__ == another.__class__:
+#             return (self.name, self.roll_no, self.major, self.year) == (
+#                 another.name,
+#                 another.roll_no,
+#                 another.major,
+#                 another.year,
+#             )
+#         else:
+#             return "InvalidComparison"
+
+from dataclasses import dataclass
+
+@dataclass
 class Student:
-    def __init__(self, name, roll_no, major, year):
-        self.name = name
-        self.roll_no = roll_no
-        self.major = major
-        self.year = year
-
-    def __repr__(self):
-        return f"Student: {self.name} {self.roll_no} {self.major} {self.year}"
-
-    def __eq__(self, another):
-        if self.__class__ == another.__class__:
-            return (self.name, self.roll_no, self.major, self.year) == (
-                another.name,
-                another.roll_no,
-                another.major,
-                another.year,
-            )
-        else:
-            return "InvalidComparison"
+    name: str
+    roll_no: str
+    major: str
+    year: str
+    gpa: float
