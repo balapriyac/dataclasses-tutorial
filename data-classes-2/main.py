e233225d04b2c2abdf9256c8552a4f5111c2907c
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2023-04-08 12:33:51 +0530

    Update main.py

--- data-classes-2/main.py
+++ data-classes-2/main.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import random
+random.seed(42)
 import string
 
 alphabet = string.ascii_uppercase + string.digits
@@ -32,7 +33,7 @@ joy = Student('Joy','Smith','Political Science','sophomore',4.00)
 
 instance_list = [jane,julia,jake,joy]
 instance_list.sort()
-pprint(instance_list)
+# pprint(instance_list)
 
 for instance in instance_list:
     print(f"{instance.first_name} {instance.last_name}'s tuition: {instance.tuition}")
@@ -40,3 +41,24 @@ for instance in instance_list:
 #print(julia > jane)
 #print(getmembers(Student,isfunction))
 #pprint(getmembers(Student,isfunction))
+
+@dataclass
+class TA(Student):
+    course: str = None
+    hours_per_week: int = 0
+    stipend: int = 100
+
+# Example for __slots__
+# @dataclass(slots=True)
+# class StudentSlots:
+#     first_name: str
+#     last_name: str
+#     major: str
+#     year: str
+#     gpa: float
+#     roll_num: str = field(default_factory=generate_roll_num, init=False)
+#     email:str = field(init=False)
+#     tuition:int = 10000
+
+#     def __post_init__(self):
+#         self.email = f"{self.first_name}.{self.last_name}@uni.edu"
