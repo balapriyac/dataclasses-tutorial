b789074bf5d672a8979ca053bd58ff74ba02060e
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2023-06-04 13:41:40 +0530

    Update main.py

--- dataclasses-vs-namedtuples/main.py
+++ dataclasses-vs-namedtuples/main.py
@@ -51,15 +51,13 @@ print(book3.title)
 print(book3_copy.title)
 
 # typing.NamedTuple
-
+from typing import NamedTuple
 # with type hints
 BookNT = NamedTuple('BookNT',[('title',str),('author',str),('genre',str),('standalone',bool)])
 book = BookNT('Six of Crows','Leigh Bardugo','Fantasy',False)
 print(book)
 
 # using class syntax
-from typing import NamedTuple
-
 class BookNT(NamedTuple):
     title:str
     author:str
@@ -69,6 +67,32 @@ class BookNT(NamedTuple):
 book = BookNT('Six of Crows','Leigh Bardugo','Fantasy',False)
 print(book)
 
-# Suclass checks
+# Subclass checks
 print(issubclass(BookNT,NamedTuple))
 print(issubclass(BookNT,tuple))
+
+# memory & attribute access
+from pympler.asizeof import asizeof
+
+book_dc = BookDC('Hyperfocus','Chris Bailey','Nonfiction',True)
+book_nt = BookNT('Hyperfocus','Chris Bailey','Nonfiction',True)
+
+s1 = asizeof(book_dc)
+s2 = asizeof(book_nt)
+
+print(f"Size of BookDC data class: {s1}")
+print(f"Size of BookNT named tuple: {s2}")
+
+# set slots to True in the decorator
+book_dc_slots = BookDC('Hyperfocus','Chris Bailey','Nonfiction',True)
+
+from functools import partial
+import timeit
+def get(book):
+    book.title
+
+t1 = min(timeit.repeat(partial(get,book_dc)))
+t2 = min(timeit.repeat(partial(get,book_nt)))
+
+print(f"Attribute access time for data class instance: {t1:.2f}")
+print(f"Attribute access time for named tuple instance: {t2:.2f}")
